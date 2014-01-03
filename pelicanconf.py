0ee55bd73369c69638a401f4beef551a79bc820c
Jon Chen <bsd@voltaire.sh>
2014-01-03 14:23:23 -0400

    remove unneeded conf options

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,19 +30,6 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-# PLUGINS = ['pelican_youtube',]
-
-
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
